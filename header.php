ff801c24ffc60942ad3e54274592dc28e84b913f
Mal <44848986+malachiperrin@users.noreply.github.com>
2023-03-12 23:02:43 -0400

    Fixed typo in blog method for site title

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo bloginfo("naem"); ?></title>
+    <title><?php echo bloginfo("name"); ?></title>
 
     <!-- bulma -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
